afd350348ce0f8d8f6c14672cd8660355557d794
andreyrisukhin <andrey.risukhin@outlook.com>
2026-02-09 07:17:02 +0000

    Deploying to gh-pages from @ andreyrisukhin/andreyrisukhin.github.io@dedbb82e68c03ecce423b47f475c2344d7e192c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -112,6 +112,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=6OrsxqIAAAAJ", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/AndreyRisuka", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
