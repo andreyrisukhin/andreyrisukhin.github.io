6b815ac7fbabcb84e24f7bd57700620517846fc3
andreyrisukhin <andrey.risukhin@outlook.com>
2026-05-24 18:54:56 +0000

    Deploying to gh-pages from @ andreyrisukhin/andreyrisukhin.github.io@38f2603ca40b1339d8e7a26f20f5c7b72119e4d1 🚀

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * "new version available" prompt surfaced by pwa.liquid.
  */
 
-const VERSION = '1779648361';
+const VERSION = '1779648858';
 const CACHE_STATIC = 'music-pwa-static-' + VERSION;
 const CACHE_RUNTIME = 'music-pwa-runtime-' + VERSION;
 
